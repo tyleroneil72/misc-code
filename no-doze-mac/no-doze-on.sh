0bf63cc3a042dcbb9a28a8fc44f7af73443f0916
Tyler O'Neil <tyleroneildev@gmail.com>
2024-01-24 12:20:35 -0500

    Add on code

--- no-doze-mac/no-doze-on.sh
+++ no-doze-mac/no-doze-on.sh
@@ -1,1 +1,8 @@
 # Turn on No Doze
+
+# Use this to check the hibernation mode of the system
+# pmset -g | grep hibernatemode
+
+sudo pmset -a sleep 0
+sudo pmset -a hibernatemode 0
+sudo pmset -a disablesleep 1
