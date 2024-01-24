6a2519c1ca3e24992048549094f732c49e735f3a
Tyler O'Neil <tyleroneildev@gmail.com>
2024-01-24 12:21:55 -0500

    Add off code

--- no-doze-mac/no-doze-off.sh
+++ no-doze-mac/no-doze-off.sh
@@ -1,1 +1,10 @@
 # Turn off No Doze
+sudo pmset -a sleep 1
+sudo pmset -a hibernatemode 
+sudo pmset -a disablesleep 0
+
+
+sudo pmset -a sleep 1
+# Use the original hibernation mode (3 on macbook)
+sudo pmset -a hibernatemode 3
+sudo pmset -a disablesleep 0
